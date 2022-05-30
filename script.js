534221a6fc2132bb1f0d8f55a65db53763ac1ceb
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-05-30 17:17:26 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-function selected (event) {
-  const colors = document.getElementsByClassName('color');
-  event.target.className = "color selected";
-
-  for (let element of colors) {
-    if (element !== event.target) {
-      element.className = "color"
-    };
-  };
-
-};
-
-const colors = document.getElementsByClassName('color');
-for (let element of colors) {
-  element.addEventListener('click', selected);
-};
-
-
-
-function changeColor (event) {
+function changeColorPixel (event) {
   let color = document.getElementsByClassName('selected');
   event.target.style.background = color[0].style.backgroundColor;
   console.log(color[0])
 }
-
-const pixels = document.getElementsByClassName('pixel');
-for (let element of pixels) {
-  element.addEventListener('click', changeColor);
-};
-
 
 
 function clearBoard (event) {
@@ -37,5 +12,67 @@ function clearBoard (event) {
   };
 }
 
-const button = document.getElementById('clear-board');
-button.addEventListener('click', clearBoard);
+
+function createBoard (value) {
+  const pixelsBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < value; index += 1) {
+    let div = document.createElement('div');
+    pixelsBoard.appendChild(div);
+    for (let index = 0; index < value; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.addEventListener('click', changeColorPixel);
+      pixel.className = 'pixel';
+      div.appendChild(pixel);
+    };
+  }
+}
+
+
+function selectedColor (event) {
+  const colors = document.getElementsByClassName('color');
+  event.target.className = "color selected";
+
+  for (let element of colors) {
+    if (element !== event.target) {
+      element.className = "color";
+    };
+  };
+
+}
+
+
+function changeBoardSize (event) {
+  const input = document.getElementById('board-size');
+  const pixelsBoard = document.getElementById('pixel-board');
+
+  if (parseInt(input.value) >= 5 && parseInt(input.value) <= 50) {
+    pixelsBoard.innerText = '';
+    createBoard(input.value);
+  } else if (parseInt(input.value) < 5 && parseInt(input.value) !== 0) {
+    pixelsBoard.innerText = '';
+    createBoard(5);
+  } else if (parseInt(input.value) > 50) {
+    pixelsBoard.innerText = '';
+    createBoard(50);
+  } else {
+    alert('Board inválido!');
+  };
+};
+
+
+
+// Selected color
+const colors = document.getElementsByClassName('color');
+for (let element of colors) {
+  element.addEventListener('click', selectedColor);
+}
+
+// Button clear board
+const buttonClearBoard = document.getElementById('clear-board');
+buttonClearBoard.addEventListener('click', clearBoard);
+
+// Button change board size
+const buttonChangeBoardSize = document.getElementById('generate-board');
+buttonChangeBoardSize.addEventListener('click', changeBoardSize);
+
+createBoard(5);
